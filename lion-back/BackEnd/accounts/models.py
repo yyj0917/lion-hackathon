44813e02fbc0bf2fbd4ce8339077cfb026dd9794
LEE <sw09112@yonsei.ac.kr>
2024-07-17 22:44:19 +0900

    venv,accounts

--- lion-back/BackEnd/accounts/models.py
+++ lion-back/BackEnd/accounts/models.py
@@ -1,54 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
     # 일반 user 생성
-    def create_user(self, email, nickname, name, password=None):
+    def create_user(self, email, password, **kwargs):
         if not email:
             raise ValueError('must have user email')
-        if not nickname:
-            raise ValueError('must have user nickname')
-        if not name:
-            raise ValueError('must have user name')
         user = self.model(
-            email = self.normalize_email(email),
-            nickname = nickname,
-            name = name
+            email = email
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     # 관리자 user 생성
-    def create_superuser(self, email, nickname, name, password=None):
-        user = self.create_user(
-            email,
-            password = password,
-            nickname = nickname,
-            name = name
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        superuser = self.create_user(
+            email = email,
+            password=password,
         )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+            
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
+        
+        superuser.save(using=self._db)
+        return superuser
 
-class User(AbstractBaseUser):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
-    nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
-    name = models.CharField(default='', max_length=100, null=False, blank=False)
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=30, null=False, blank=False, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     
-    # User 모델의 필수 field
-    is_active = models.BooleanField(default=True)    
-    is_admin = models.BooleanField(default=False)
-    
-    # 헬퍼 클래스 사용
+    # 헬퍼 클래스 사용, 위에서 만들어 둔 거
     objects = UserManager()
 
     # 사용자의 username field는 nickname으로 설정
-    USERNAME_FIELD = 'nickname'
-    # 필수로 작성해야하는 field
-    REQUIRED_FIELDS = ['email', 'name']
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return self.nickname
+        return self.email
 
--- lion-back/BackEnd/accounts/serializers.py
+++ lion-back/BackEnd/accounts/serializers.py
@@ -5,11 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             email = validated_data['email'],
-            nickname = validated_data['nickname'],
-            name = validated_data['name'],
             password = validated_data['password']
         )
         return user
     class Meta:
         model = User
-        fields = ['nickname', 'email', 'name', 'password']
+        fields = '__all__'
