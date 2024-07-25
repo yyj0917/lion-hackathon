02f8887d3ca51c493fd006dfa9c1b422422d7e22
yyj0917 <yyj0917@yonsei.ac.kr>
2024-07-25 14:33:40 +0900

    FE mypage design

--- lion-front/src/components/MyPage/MyPage.js
+++ lion-front/src/components/MyPage/MyPage.js
@@ -1,7 +1,7 @@
-import { BarChart4, CircleUser, Laugh, Notebook, SquarePen } from "lucide-react";
+import { BarChart4, CircleUser, Flame, Laugh, Notebook, SquarePen } from "lucide-react";
 import styled from "styled-components";
 import { Outlet, useNavigate } from "react-router-dom";
-
+import profile from "../../assets/profileimages.png";
 
 const Wrapper = styled.div`
 
@@ -17,50 +17,106 @@ const Wrapper = styled.div`
 
 `;
 const Navbar = styled.div`
-    width: 20%;
-    height: auto;
+    width: 30%;
+    height: 90%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    border-radius: 20px;
-    margin-right: 5px;
     align-items: center;
-    p {
-        cursor: pointer;
-        box-shadow: 0 0px 5px rgba(0,0,0,0.1);
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #f44336;
-        &:hover {
-            background-color: #f7f7f7;
-        }
-    }
+    gap: 20px;
+    padding: 20px 0;
+    
 `;
 const Contents = styled.div`
     margin-left: 5px;
     width: 75%;
-    height: 90%;
-    box-shadow: 0 0 10px rgba(0,0,0,0.1);
-    border-radius: 20px;
+    height: 100%;
+    border-radius: 0 20px 20px 0;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: rgba(220, 220, 220, 0.3); /* 흐린 회색 배경색 */
+
+`;
+const ImgWrapper = styled.div`
+    width: 100%;
+    height: 30%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+        border-radius: 50%;
+        width: 50%;
+        height: 50%;
+    }
+    p {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        font-family: "Dongle", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 25px;
+    
+    }
+
+`;
+const IconWrapper = styled.div`
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .profile-text {
+        display: flex;
+        align-items: center; /* 세로 정렬 */
+        margin-left: 10px; /* 아이콘과 텍스트 간의 간격 */
+        font-size: 16px;
+        line-height: 1.5;
+
+    }
+    p {
+        cursor: pointer;
+        position: relative;
+        font-family: "Dongle", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 25px;
+        width: 100px;
+        height: 10px;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        color: #f44336;
+        gap: 5px;
+        &:hover {
+            background-color: #f7f7f7;
+        }
+
+    }
 `;
 export default function MyPage() {
     const navigate = useNavigate();
     return (
         <Wrapper>
             <Navbar>
-                <p onClick={() => navigate('/mypage')}><CircleUser/></p>
-                <p onClick={() => navigate('/mypage/diary')}><Notebook/></p>
-                <p onClick={() => navigate('/mypage/sentiment')}><Laugh/></p>
-                <p onClick={() => navigate('/mypage/sentimentResult')}><BarChart4/></p>
-                <p onClick={() => navigate('/mypage/writeDiary')}><SquarePen/></p>
-            </Navbar>
+                <ImgWrapper>
+                    <img src={profile} alt="profile"/>
+                    <div className="profile-text">
+                        
+                        <p><Flame strokeWidth={4} style={{color: "#f44336", display:"flex", marginTop:"10%"}} />윤영준(25) <br /> 경기도 구리시 소방청</p>
+                    </div>
+                </ImgWrapper>
+                <IconWrapper>
+                    <p onClick={() => navigate('/mypage')}>
+                        <CircleUser/> <span>Profile</span></p>
+                    <p onClick={() => navigate('/mypage/diary')}><Notebook/> Diary</p>
+                    <p onClick={() => navigate('/mypage/sentiment')}><Laugh/> Feel</p>
+                    <p onClick={() => navigate('/mypage/sentimentResult')}><BarChart4/> Analyze</p>
+                    <p onClick={() => navigate('/mypage/writeDiary')}><SquarePen/> Write</p>
+                </IconWrapper>
+                </Navbar>
             <Contents>
                 <Outlet/>
             </Contents>
