d103228e4a1ea66080ae8f5f7ccec56aa8d45e17
jimin <jm0903@yonsei.ac.kr>
2024-07-26 02:15:23 +0900

    matching first setting and models

--- lion-back/BackEnd/match/views.py
+++ lion-back/BackEnd/match/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.db.models import Count
+import random
 
-# Create your views here.
+from .models import Adviser, Client
+from .serializers import AdviserSerializer, ClientSerializer
+
+
+class AdviserViewSet(viewsets.ModelViewSet):
+    queryset = Adviser.objects.all()
+    serializer_class = AdviserSerializer
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
